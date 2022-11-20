c52453d73593cde1d30cc8013cccdf4dd54e58fb
BleakCodex <BleakCodex@gmail.com>
2022-11-19 20:03:43 -0800

    Initial Commit

---
+++ Content.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework.Audio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSounds
+{
+    public class Content
+    {
+        public List<Sound>? Sounds { get; set; }
+    }
+
+    public class Sound
+    {
+        public string? Name { get; set; }
+        public string? FromFile { get; set; }
+        public bool Loop { get; set; } = false;
+        public int InstanceLimit { get; set; } = 1;
+        public CueDefinition.LimitBehavior LimitBehavior { get; set; } = CueDefinition.LimitBehavior.FailToPlay;
+    }
+}
